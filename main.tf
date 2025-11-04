64530827dfbafc67c679ea16b307b8002d77566b
sharmila <sharmeeh@gmail.com>
2025-11-04 18:58:26 +1100

    changed name prefix

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "storageacc" {
   source            = "app.terraform.io/Sharmila/storageacc/azurerm"
   version           = "1.0.0"
-  name_prefix       = "sharmi"  
+  name_prefix       = "mithy"  
   location          = var.location
   account_replication_type = "LRS"
 }
