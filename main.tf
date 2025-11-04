926e371d5d2471df7c4c7ff7f8a72e23814eafb4
sharmila <sharmeeh@gmail.com>
2025-11-04 18:10:07 +1100

    using 1.0.0 ver of module

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "storageacc" {
   source            = "app.terraform.io/Sharmila/storageacc/azurerm"
-  version           = "1.0.1"
+  version           = "1.0.0"
   name_prefix       = "sharmi"  
   location          = var.location
   account_replication_type = "LRS"
