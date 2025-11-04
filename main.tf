fe9b44e07ecee53858600739b951ad0ea86d7faa
sharmila <sharmeeh@gmail.com>
2025-11-04 18:07:37 +1100

    used staorageacc tf module

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-module "storageAccount" {
-  source            = "app.terraform.io/Sharmila/storageAccount/azurerm"
+module "storageacc" {
+  source            = "app.terraform.io/Sharmila/storageacc/azurerm"
   version           = "1.0.1"
   name_prefix       = "sharmi"  
   location          = var.location
+  account_replication_type = "LRS"
 }
